a51cc154790cecdb04050a2101c76c65b18af1cf
kdha0528 <71540936+kdha0528@users.noreply.github.com>
2021-01-09 18:05:40 +0900

    JS scrolling change navbar's background color

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(navbarHeight);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar__dark');
+    } else {
+        navbar.classList.remove('navbar__dark');
+    }
+});
