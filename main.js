8a3692cbb42f6e071cf3a07f0e730f6210283f2d
kdha0528 <71540936+kdha0528@users.noreply.github.com>
2021-01-10 18:29:28 +0900

    JS scroll into view

--- main.js
+++ main.js
@@ -5,11 +5,22 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(navbarHeight);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar__dark');
     } else {
         navbar.classList.remove('navbar__dark');
     }
+});
+// Handle scrolling when tapping on the navbar menu.
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
