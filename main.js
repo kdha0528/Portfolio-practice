31ff4a10bf537134a5bf3a393d8db69378ddf308
kdha0528 <71540936+kdha0528@users.noreply.github.com>
2021-01-15 16:10:14 +0900

    JS Projects image and button

--- main.js
+++ main.js
@@ -43,6 +43,51 @@ arrow.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
+//Project image and button
+const img = document.querySelectorAll('.project__img');
+const right = document.querySelector('.project__rightBtn');
+const left = document.querySelector('.project__leftBtn');
+const first = 0;
+var index = 0;
+right.addEventListener('click', () => {
+    if (index == 0) {
+        img[first].classList.add('project__invisible');
+    }
+    if (index == img.length - 1) {
+        removeProjectImg(img[index]);
+        index = 0;
+        img[first].classList.remove('project__invisible');
+        addProjectImg(img[index]);
+    } else {
+        removeProjectImg(img[index]);
+        index++;
+        addProjectImg(img[index]);
+    }
+});
+left.addEventListener('click', () => {
+    if (index == 1) {
+        img[first].classList.remove('project__invisible');
+    }
+    if (index == 0) {
+        img[first].classList.add('project__invisible');
+        removeProjectImg(img[index]);
+        index = img.length - 1;
+        addProjectImg(img[index]);
+    } else {
+        removeProjectImg(img[index]);
+        index--;
+        addProjectImg(img[index]);
+    }
+});
+
+function addProjectImg(img) {
+    img.classList.add('project__visible');
+}
+
+function removeProjectImg(img) {
+    img.classList.remove('project__visible');
+}
+
 function scrollingFadeIn(id) {
     const selector = document.querySelector(id);
     const selectorHeight = selector.getBoundingClientRect().height;
